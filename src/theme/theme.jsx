0d80423c8324ae3ab01c6afa5c1a681c07f47db1
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2023-08-20 16:48:11 +0300

    add header; add theme for mui

---
+++ src/theme/theme.jsx
@@ -0,0 +1,23 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {},
+  typography: {},
+  breakpoints: {},
+  components: {},
+});
+
+// export const theme = () => {
+//   return createTheme({
+//     palette,
+//     typography,
+//     breakpoints,
+//     components,
+//     header: {
+//       mobile: { minHeight: 112 },
+//       desktop: {
+//         minHeight: 216,
+//       },
+//     },
+//   });
+// };
